1c84269f89cc4a0fcdad1db5bea7c15778b72865
Larry Pedigo <larry.pedigo@powerhealthondemand.com>
2017-10-19 18:51:48 -0600

    Fetch by ID

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 //global vars
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectId} = require ('mongodb'); //mongodb returns a lot of useful utility methods, for instance, can determine
+
 
 //local vars
 var {mongoose} = require('./db/mongoose');
 var {Users} = require('./models/user');
 var {Todo} = require('./models/todo');
+
 
 
 var app = express();
@@ -35,6 +38,32 @@ app.get('/todos', (req,res)=>{
   }
 });
 
+// GET /todos/1234 example: localhost:3000/todos/59e377f709b0959440aff47e
+app.get('/todos/:id', (req,res)=>{
+  var id= req.params.id;
+
+  if(!ObjectId.isValid(id)){
+      return res.status(404).send({text:'ID you provided was not valid'});
+}
+  Todo.findById(id).then((todo)=>{
+      if(todo){
+        console.log('found!');
+        res.send(todo);
+      }else{
+        console.log('not found!');
+        res.status(400).send();
+      }
+}), (e)=>{
+  res.status(400).send();
+}
+
+  //findById to fund
+    //success
+
+        //if no todo - send back 404, with an empty body
+    //error
+        //4000
+});
 
 app.listen(3000, ()=>{
   console.log('Started on port 3000')
