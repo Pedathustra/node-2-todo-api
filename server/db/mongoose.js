a717814f32d70f2b38b7af0ab5fc944c6c60dcdd
Larry Pedigo <larry.pedigo@powerhealthondemand.com>
2017-10-21 11:42:39 -0600

    set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,6 @@ mongoose.Promise = global.Promise;
 //Mongoose maintains connection over time
 //if you save, mongoose will be ready with a connection waiting
 //we're going to use Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_UI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports={mongoose};
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const {ObjectId} = require ('mongodb'); //mongodb returns a lot of useful utilit
 var {mongoose} = require('./db/mongoose');
 var {Users} = require('./models/user');
 var {Todo} = require('./models/todo');
-
+const port = process.env.PORT || 3000;
 
 
 var app = express();
@@ -59,8 +59,8 @@ app.get('/todos/:id', (req,res)=>{
 
 });
 
-app.listen(3000, ()=>{
-  console.log('Started on port 3000')
+app.listen(port, ()=>{
+  console.log('Started on port ${port}')
 });
 
 module.exports ={app};
