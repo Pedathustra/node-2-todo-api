7f615d175a339c659a6fc13f2e4c17e20bddc662
Larry Pedigo <larry.pedigo@powerhealthondemand.com>
2017-11-03 17:16:04 -0600

    Has user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,26 @@
 const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken')
-;
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+var password = '123abc!';
+// to hash a password, you have to call 2 methods //gensalt //hashmethod
+//asychronous (i.e. callback), first argument is the number of rounds. the bigger the number the more difficult to crack
+//bcrypt is inherently slow, which is good
+// 1. this gnerates the pwd.
+bcrypt.genSalt(10,  (err,salt)=>{
+  bcrypt.hash(password, salt, (err, hash)=>{
+        console.log(hash)
+  });
+});
+
+// 2. next we have to see if the password matches that which is passed in from the client
+var hashedPassword = '$2a$10$K2p5tI1Xkp4LbPoFJEYUduA27m1aIzSccu6JEIO8DXyaYGCpriPGy';
+
+bcrypt.compare(password, hashedPassword, (err, res)=>{
+  console.log(res);
+});
+
+/*
 var data = { // this simulates the id of the user in our system.
   id: 10
 };
@@ -10,6 +29,7 @@ var token = jwt.sign(data,'123abc'); // 123abc is the salt secret
 console.log(token);
 var decoded = jwt.verify(token, '123abc');
 console.log(decoded);
+*/
 //jwt.verify
 /*
 // THIS IS A JSON WEB TOKEN (JWT). It's a standard. There's a package for that already
